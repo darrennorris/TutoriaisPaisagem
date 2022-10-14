ef651d1b0e31914e04582d4c857861324211f9d3
Darren Norris <doon75@hotmail.com>
2022-10-14 17:21:06 -0300

    update metrics

---
+++ R/tutorial_links.R
@@ -0,0 +1,9 @@
+
+#geocomputation with R: 
+#https://geocompr.robinlovelace.net/index.html.
+#com capitulo focado na ecologia: https://geocompr.robinlovelace.net/eco.html
+
+#E exemplos com foco em genética das paisagems 
+#https://bookdown.org/hhwagner1/LandGenCourse_book/
+  
+#landscapemetrics : https://bookdown.org/hhwagner1/LandGenCourse_book/WE_2.html
